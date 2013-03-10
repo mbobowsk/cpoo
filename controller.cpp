491b8b9ef433459ccd21a4c8a19d2f8d75917a3f
dvdu <dvdu90@gmail.com>
2013-03-10 12:24:44 +0100

    kolejna proba

--- controller.cpp
+++ controller.cpp
@@ -47,6 +47,8 @@ QImage* Controller::median(QImage *oldImg) {
                     lowIndex = i;
             }
             // Zapis 'medianowego' piksela do nowego obrazka
+            //          qDebug()<<"Low index"<<lowIndex;
+            //          qDebug()<<"Low value"<<pixels[lowIndex];
             newImg->setPixel(x,y,pixels[lowIndex]);
         }
     }    
@@ -64,26 +66,69 @@ QImage* Controller::adaptMedian(QImage *oldImg) {
     // Wysokość i szerokość obrazka
     int height = oldImg->height();
     int width = oldImg->width();
-    // Skopiowanie pikseli z granicy
-    fillBorders(oldImg,newImg,1);
-    int size = 3;
-    int border = size/2;
-    for ( int x = border; x != width-border; ++x ) {
-        for ( int y = border; y != height-border; ++y ) {
-            // Okno analizy
-            std::vector<QRgb> pixels;
-            for ( int i = x-border; i <= x+border; ++i ) {
-                for ( int j = y-border; j <= y+border; ++j ) {
-                    pixels.push_back(oldImg->pixel(i,j));
-                }
-            }
-            // Wartości sumy odległości od pozostałych pikseli
-            std::vector<int> distance;
-            for ( unsigned i = 0; i != pixels.size(); ++i )
-                distance.push_back(countDst(pixels,i));
+    // sparametryzować
+    int windowSize = 3;
+    int maxWindowSize = 11;
+
+    for ( int x = 0; x < width; ++x ) {
+        for ( int y = 0; y < height; ++y ) {
+            newImg->setPixel(x, y, adaptivePixel(oldImg, x, y, windowSize, maxWindowSize));
         }
     }
     return newImg;
+}
+
+QRgb Controller::adaptivePixel(QImage *original, int x, int y, int windowSize, int maxWindowSize){
+    // Okno analizy
+    std::list<QRgb> pixels;
+    for ( int i = x-(windowSize/2); i <= x+(windowSize/2); ++i ) {
+        for ( int j = y-(windowSize/2); j <= y+(windowSize/2); ++j ) {
+            if (i < 0 || i == original->width() || y < 0 || y == original->height())
+                continue;
+            pixels.push_back(original->pixel(i,j));
+        }
+    }
+    // Szukaj min / max / median
+    QRgb fmin, fmed, fmax, forig;
+    pixels.sort(adaptiveComparator);
+    std::list<QRgb>::iterator it = pixels.begin();
+    fmin = *it;
+    std::advance(it, (pixels.size()/2 + 1));
+    fmed = *it;
+    std::advance(it, pixels.size());
+    fmax = *it;
+
+    forig = original->pixel(x, y);
+
+    // Pierwszy warunek - czy mediana jest różna od skrajności
+    if ((qRed(fmin) + qBlue(fmin) + qGreen(fmin))/3 < (qRed(fmed) + qBlue(fmed) + qGreen(fmed))/3 && (qRed(fmed) + qBlue(fmed) + qGreen(fmed))/3 < (qRed(fmax) + qBlue(fmax) + qGreen(fmax))/3){
+        // Drugi warunek - czy oryginalny jest różny od skrajności
+        if((qRed(fmin) + qBlue(fmin) + qGreen(fmin))/3 < (qRed(forig) + qBlue(forig) + qGreen(forig))/3 && (qRed(forig) + qBlue(forig) + qGreen(forig))/3 < (qRed(fmax) + qBlue(fmax) + qGreen(fmax))/3){
+            return forig;
+        }
+        else
+        {
+            return fmed;
+        }
+    }
+    else
+    {
+        // Sprawdź, czy nowe okno analizy nie będzie za duże
+        if ((windowSize + 2) < maxWindowSize){
+            // Uruchom rekurencyjnie z większym oknem analizy
+            return adaptivePixel(original, x, y, windowSize+2, maxWindowSize);
+        }
+        else
+        {
+            return fmed;
+        }
+    }
+}
+
+bool Controller::adaptiveComparator(const QRgb first, const QRgb second){
+    int firstGray = (qRed(first) + qBlue(first) + qGreen(first))/3;
+    int secondGray = (qRed(second) + qBlue(second) + qGreen(second))/3;
+    return (firstGray < secondGray);
 }
 
 int Controller::countDst(std::vector<unsigned>& pixels, int index) {
@@ -97,6 +142,15 @@ int Controller::countDst(std::vector<unsigned>& pixels, int index) {
                     pow(qGreen(pixels[i])-qGreen(pixels[index]),2));
     }
     return ret;
+}
+
+// TODO remove, not used
+double Controller::countLength(QRgb pixel) {
+
+    return sqrt(pow(qRed(pixel)-qRed(pixel),2) +
+                pow(qBlue(pixel)-qBlue(pixel),2) +
+                pow(qGreen(pixel)-qGreen(pixel),2));
+
 }
 
 void Controller::fillBorders(QImage *oldImg, QImage *newImg, int border) {
--- controller.h
+++ controller.h
@@ -17,6 +17,12 @@ private:
     static bool sortRgb(QRgb a, QRgb b);
     // Oblicza sumę odległości piksela o podanym indeksie
     int countDst(std::vector<unsigned>& pixels, int index);
+    // Obicza gługość wektora trzech  wymiarów (RGB)
+    double countLength(QRgb pixel);
+    // Oblicza wartość zadanego pixela przy określonym rozmiarze okna
+    QRgb adaptivePixel(QImage *original, int x, int y, int windowSize, int maxWindowSize);
+    // Komparator do sortowania listy pikseli
+    static bool adaptiveComparator(const QRgb first, const QRgb second);
     // Wypełnia granice nowego obrazka pikselami ze starego obrazka
     void fillBorders(QImage *oldImg, QImage *newImg, int border);
 };
